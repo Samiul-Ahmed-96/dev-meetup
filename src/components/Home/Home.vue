3327f172d254be9b49f55dbbdd3def0b4be2514a
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-04-26 15:31:38 +0600

    Added Meetups

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,5 +1,6 @@
 <template>
-    <v-container>
+    <v-container class="text-center">
+    <h1 class="teal darken-4 d-inline-block elevation-8 py-2 px-8 rounded my-8 white--text text-uppercase font-weight-bold">Join Our Awesome Meetup</h1>
     <v-carousel class="my-12 rounded">
     <v-carousel-item
       v-for="(item,i) in items"
@@ -8,7 +9,7 @@
       reverse-transition="fade-transition"
       transition="fade-transition"
       
-    ></v-carousel-item>
+    ><h2 class="text-center teal darken-4 white--text text-uppercase font-weight-thin">{{item.name}}</h2></v-carousel-item>
   </v-carousel>
         <v-btn outlined large to="meetups" class="teal darken-4 white--text">Explore Meetups</v-btn>
         <v-btn outlined large to="meetup/new" class="teal darken-4 white--text ml-6">Organize Meetup</v-btn>
@@ -23,17 +24,21 @@
       return {
         items: [
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            name:'New York City',
+            id:'123123123423452',
+            src: 'https://images.pexels.com/photos/290386/pexels-photo-290386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            name:'Tokyo Japan',
+            id:'1231231232343452',
+            src: 'https://images.pexels.com/photos/2187605/pexels-photo-2187605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            name:'Netherlands',
+            id:'123123123423452',
+            src: 'https://images.pexels.com/photos/745243/pexels-photo-745243.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
           },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
+         
         ],
       }
     },
---
+++ src/components/Meetups/Meetups.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container>
+    <div class="heading text-center">
+      <h1
+        class="
+          teal
+          darken-4
+          d-inline-block
+          elevation-8
+          py-2
+          px-8
+          rounded
+          my-8
+          white--text
+          text-uppercase
+          font-weight-bold
+        "
+      >
+        Explore Our Meetups
+      </h1>
+    </div>
+    <v-row>
+      <v-col md="6" offset-md="3" sm="12">
+        <v-card elevation="6" class="mx-auto my-6" max-width="768">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn color="orange lighten-2" text> Explore </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                I'm a thing. But, like most politicians, he promised more than
+                he could deliver. You won't have time for sleeping, soldier, not
+                with all the bed making you'll be doing. Then we'll go with that
+                data file! Hey, you add a one and two zeros to that or we walk!
+                You're going to do his laundry? I've got to find a way to
+                escape.
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "meetups-area",
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
+
+<style  scoped>
+</style>
